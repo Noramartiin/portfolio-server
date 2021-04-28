3f1f60744d70cc7a8634507ce93d68573660eab0
Nora Martin <noramartiin96@gmail.com>
2021-04-28 15:01:14 +0200

    prueba

--- routes/index.js
+++ routes/index.js
@@ -6,18 +6,18 @@ router.get("/", (req, res, next) => {
   res.render("index");
 });
 
-// router.get("/contact", (req, res) => {
-//   ContactModel.find()
-//     .then((response) => {
-//       res.status(200).json(response);
-//     })
-//     .catch((err) => {
-//       res.status(500).json({
-//         error: "Something went wrong",
-//         message: err,
-//       });
-//     });
-// });
+router.get("/contact", (req, res) => {
+  ContactModel.find()
+    .then((response) => {
+      res.status(200).json(response);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        error: "Something went wrong",
+        message: err,
+      });
+    });
+});
 
 router.post("/contact/create", (req, res) => {
   const { name, email, subject, message } = req.body;
